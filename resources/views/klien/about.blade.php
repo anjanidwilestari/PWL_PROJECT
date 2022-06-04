787d5459419e8375172816d53120a1996f185202
Ayi <ayianjani774@gmail.com>
2022-06-04 12:17:36 +0700

    update view about tampilan klien

--- resources/views/klien/about.blade.php
+++ resources/views/klien/about.blade.php
@@ -1,4 +1,4 @@
-@extends('klienLayouts.main')
+@extends('klienLayouts.main2')
 
 @section('title')
     {{ $title }}
@@ -15,10 +15,10 @@
                 <div class="col-md-6">
                     <div class="img_container">
                         <div class="img-box b1">
-                            <img src="{{ asset('styleKlien/images/a-1.jpg')}}" alt="" />
+                            <img src="{{ asset('styleKlien/images/a-1.jpg')}}" alt="Rental alat pendakian" />
                         </div>
                         <div class="img-box b2">
-                            <img src="{{ asset('styleKlien/images/a-2.jpg')}}" alt="" />
+                            <img src="{{ asset('styleKlien/images/a-2.jpg')}}" alt="Rental alat pendakian" />
                         </div>
                     </div>
                 </div>
@@ -26,15 +26,18 @@
                     <div class="detail-box">
                         <div class="heading_container">
                             <h3>
-                                About Our College
+                                Tentang Bolang Gunung
                             </h3>
                             <p>
-                                It is a long established fact that a reader will be distracted
-                                by the readable content of a page when looking at its layout.
-                                The point of using Lorem Ipsum is that it has a more it
+                                Bolang Gunung salah satu persewaan perlengkapan pendakian dan camping yang lengkap 
+                                di Malang yang berdiri sejak tahun 2022. Dengan harga yang sangat murah, barang kami 
+                                memiliki kualitas yang sangat bagus dan terawat sesuai standarisasi untuk persewaan, 
+                                itu semua demi kenyamanan dan keamanan anda. Jadi bagi anda yang menghabiskan waktu liburan 
+                                anda untuk pendakian dan bagi anda yang bingung ingin menyewa perlengkapan camping, kami adalah 
+                                solusi tepat untuk anda, dengan harga yang terjangkau dan mengutamakan keselamatan.
                             </p>
                             <a href="">
-                                Read More
+                                Sewa Sekarang
                             </a>
                         </div>
                     </div>
