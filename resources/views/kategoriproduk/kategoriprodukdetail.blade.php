8e86c473c61e293fd87b24a062ccc15e92c0008f
Ayi <ayianjani774@gmail.com>
2022-06-05 16:08:35 +0700

    update detail views

---
+++ resources/views/kategoriproduk/kategoriprodukdetail.blade.php
@@ -0,0 +1,72 @@
+@extends('adminLayouts.main')
+@section('title')
+    Detail Data Kategori Produk
+@endsection
+@section('content')
+    <div class="right_col" role="main">
+        <div class="">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="x_panel">
+                        <div class="x_title">
+                            <h2>Detail Data {{ $kategoriproduk->nama }}</h2>
+                            <ul class="nav navbar-right panel_toolbox">
+                                <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                                </li>
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                                        aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                                    <ul class="dropdown-menu" role="menu">
+                                        <li><a href="#">Settings 1</a>
+                                        </li>
+                                        <li><a href="#">Settings 2</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li><a class="close-link"><i class="fa fa-close"></i></a>
+                                </li>
+                            </ul>
+                            <div class="clearfix"></div>
+                        </div>
+                        <div class="x_content">
+                            <ul class="list-unstyled timeline">
+                                <div class="col-md-6">
+                                    <li>
+                                        <div class="block">
+                                            <div class="tags">
+                                                <a href="" class="tag">
+                                                    <span>Kategori</span>
+                                                </a>
+                                            </div>
+                                            <div class="block_content">
+                                                <h2 class="title">
+                                                    <a>{{$kategoriproduk->nama_kategori}}</a>
+                                                </h2>
+                                            </div>
+                                            <br>
+                                        </div>   
+                                    </li>
+                                    <li>
+                                        <div class="block">
+                                            <div class="tags">
+                                                <a href="" class="tag" width="100px">
+                                                    <span>Deskripsi</span>
+                                                </a>
+                                            </div>
+                                            <div class="block_content">
+                                                <h2 class="title">
+                                                    <a>{{$kategoriproduk->deskripsi}}</a>
+                                                </h2>
+                                            </div>
+                                            <br>
+                                        </div>   
+                                    </li>
+                                </div>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
