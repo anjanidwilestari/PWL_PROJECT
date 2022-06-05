fff65c3437cbf9f885e2edddb864d3af20a8cb79
bellasoniadwi <bellasoniadwi@gmail.com>
2022-06-05 21:38:06 +0700

    make view edit member

--- resources/views/member/memberedit.blade.php
+++ resources/views/member/memberedit.blade.php
@@ -1,15 +1,17 @@
 @extends('adminLayouts.main')
 @section('title')
-    Edit Data Pegawai
+    Edit Data Member
 @endsection
+@section('member', 'active')
 @section('content')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <div class="right_col" role="main">
         <div class="">
             <div class="row">
                 <div class="col-md-12 col-xs-12">
                     <div class="x_panel">
                         <div class="x_title">
-                            <h2>Tambah Data Pegawai <small></small></h2>
+                            <h2>Edit Data Member <small></small></h2>
                             <ul class="nav navbar-right panel_toolbox">
                                 <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                                 </li>
@@ -41,42 +43,50 @@
                         <div class="x_content">
                             <br>
                             <form class="form-horizontal form-label-left input_mask" method="POST"
-                                enctype="multipart/form-data" action="{{route('pegawai.update',$pegawai->id)}}">
+                                enctype="multipart/form-data" action="{{ route('member.update', $member->id) }}">
                                 @csrf
                                 @method('PUT')
                                 <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                    <input type="text" class="form-control has-feedback-left" placeholder="Nama Lengkap" name="nama" required value="{{$pegawai->nama}}">
+                                    <label for="fullname">Nama Lengkap * :</label>
+                                    <input type="text" class="form-control has-feedback-left" placeholder="Nama Lengkap"
+                                        name="nama" value="{{ $member->nama }}">
                                     <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
                                 </div>
 
                                 <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                    <select class="form-control" id="jabatan" name="jabatan" value="{{$pegawai->jabatan}}">
-                                        <option value="Manager">Manager</option>
-                                        <option value="Admin">Admin</option>
-                                        <option value="Karyawan">Karyawan</option>
-                                    </select>
-                                    <span class="fa fa-group form-control-feedback right" aria-hidden="true"></span>
-                                    @if ($errors->has('jabatan'))
-                                        <div class="error">{{ $errors->first('jabatan') }}</div>
-                                    @endif
+                                    <label for="fullname">Umur * :</label>
+                                    <input type="number" class="form-control has-feedback-right" placeholder="Umur"
+                                        name="umur" id="umur" value="{{ $member->umur }}">
+                                    <span class="fa fa-birthday-cake form-control-feedback right" aria-hidden="true"></span>
                                 </div>
 
                                 <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                    {{-- <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span> --}}
-                                    <input type="file" class="form-control" placeholder="Foto" name="foto" value="{{old('foto')}}">
-                                    <img width="80px" height="100"src="{{asset('storage/'. $pegawai->foto)}}" >
+                                    <label for="fullname">Alamat * :</label>
+                                    <input type="text" class="form-control has-feedback-left" placeholder="Alamat"
+                                        name="alamat" value="{{ $member->alamat }}">
+                                    <span class="fa fa-home form-control-feedback left" aria-hidden="true"></span>
                                 </div>
 
                                 <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                    <input type="date" class="form-control" placeholder="Tanggal Lahir" name="tanggal_lahir" required value="{{$pegawai->tanggal_lahir}}">
+                                    <label for="fullname">Tanggal Lahir * :</label>
+                                    <input type="date" class="form-control" placeholder="Tanggal Lahir"
+                                        name="tanggal_lahir" value="{{ $member->tanggal_lahir }}">
                                     <span class="fa fa-calendar form-control-feedback right" aria-hidden="true"></span>
                                 </div>
 
                                 <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                    <input type="text" class="form-control has-feedback-left" placeholder="Alamat" name="alamat" required value="{{$pegawai->alamat}}">
-                                    <span class="fa fa-home form-control-feedback left" aria-hidden="true"></span>
+                                    <label for="fullname">Foto KTP * :</label>
+                                    <input type="file" class="form-control" placeholder="KTP" name="ktp"
+                                        value="{{ old('ktp') }}">
+                                    <img width="80px" height="100" src="{{ asset('storage/' . $member->ktp) }}">
                                 </div>
 
+                                <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback" id="kartu_pelajar">
+                                    <label for="fullname">Kartu Pelajar * :</label>
+                                    <input type="file" class="form-control" placeholder="Kartu Pelajar"
+                                        name="kartu_pelajar" value="{{ old('kartu_pelajar') }}">
+                                        <img width="80px" height="100" src="{{ asset('storage/' . $member->kartu_pelajar) }}">
+                                </div>
                                 {{-- <div class="form-group">
           <label class="control-label col-md-3 col-sm-3 col-xs-12">Disabled Input </label>
           <div class="col-md-9 col-sm-9 col-xs-12">
@@ -95,7 +105,7 @@
                                         <br><br>
                                         <button type="submit" class="btn btn-success">Submit</button>
                                         <a class="btn btn-primary" href="{{ route('pegawai.index') }}">Cancel</a>
-                                        <a class="btn btn-danger" type="reset">Reset</a>
+                                        <button class="btn btn-danger" type="reset">Reset</button>
                                     </div>
                                 </div>
                             </form>
@@ -105,4 +115,17 @@
             </div>
         </div>
     </div>
+@endsection
+@section('js')
+    <script type="text/javascript">
+        $(function() {
+            $("#umur").change(function() {
+                if ($(this).val() < 17) {
+                    $("#kartu_pelajar").show();
+                } else {
+                    $("#kartu_pelajar").hide();
+                }
+            });
+        });
+    </script>
 @endsection
