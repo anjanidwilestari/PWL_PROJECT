9277f8c1683634961fc938d4d10ca6cef4c88c42
bellasoniadwi <bellasoniadwi@gmail.com>
2022-06-06 00:16:53 +0700

    create table get in touch

---
+++ database/migrations/2022_06_05_161301_create_get_in_touches_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('get_in_touches', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('no_hp');
+            $table->string('email');
+            $table->string('pesan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('get_in_touches');
+    }
+};
