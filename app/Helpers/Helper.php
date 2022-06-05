1541697b56d8eaa688c65683b8f63740efbcd765
bellasoniadwi <bellasoniadwi@gmail.com>
2022-06-05 15:19:04 +0700

    auto number kode member

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -3,6 +3,7 @@
 namespace App\Helpers;
 
 use App\Models\Pegawai as MasterPegawai;
+use App\Models\Member as MasterMember;
 
 class Helper{
     public static function KodePegawaiGenerator(){
@@ -13,30 +14,53 @@ class Helper{
         ->first();
 
         if (!empty($cek_kode_pegawai)) {
-
             $get_angka_akhir = substr($cek_kode_pegawai->kode_pegawai, -3);
             $get_angka_akhir = $get_angka_akhir + 1;
             // dd($get_angka_akhir);
-
             $arr_angka = [
                 '10' => '00',
                 '100' => '0',
                 '1000' => '',
             ];
             foreach ($arr_angka as $key_angka => $value_angka) {
-//
                 // dd($get_angka_akhir, $get_angka_akhir < $key_angka);
                 if ($get_angka_akhir < $key_angka) {
-
                     $kode = $kode .$value_angka. $get_angka_akhir;
                     break;
                 }
             }
         } else {
-
             $kode = $kode . '001';
         }
+        return $kode;
+    }
 
+    public static function KodeMemberGenerator(){
+        $kode = 'KM'.date('ymd') .'';
+        $cek_kode_member = MasterMember::where('kode_member', 'like', '%' . $kode . '%')
+        ->select('kode_member')
+        ->orderBy('kode_member', 'desc')
+        ->first();
+
+        if (!empty($cek_kode_member)) {
+            $get_angka_akhir = substr($cek_kode_member->kode_member, -3);
+            $get_angka_akhir = $get_angka_akhir + 1;
+            // dd($get_angka_akhir);
+            $arr_angka = [
+                '10' => '00',
+                '100' => '0',
+                '1000' => '',
+            ];
+            foreach ($arr_angka as $key_angka => $value_angka) {
+                // dd($get_angka_akhir, $get_angka_akhir < $key_angka);
+                if ($get_angka_akhir < $key_angka) {
+                    $kode = $kode .$value_angka. $get_angka_akhir;
+                    break;
+                }
+            }
+        } else {
+            $kode = $kode . '001';
+        }
         return $kode;
     }
 }
