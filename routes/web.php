0e209858bdce53f7a02db9b073eb92fc025b0715
bellasoniadwi <bellasoniadwi@gmail.com>
2022-06-07 12:21:53 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,14 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\GetInTouchController;
 use App\Http\Controllers\KategoriProdukController;
 use App\Http\Controllers\KlienController;
 use App\Http\Controllers\MemberController;
 use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -20,24 +22,27 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/klien-beranda', [KlienController::class, 'home']);
-Route::get('/klien-produk', [KlienController::class, 'produk']);
-Route::get('/klien-galery', [KlienController::class, 'galery']);
-Route::get('/klien-about', [KlienController::class, 'about']);
-Route::resource('contact', GetInTouchController::class);
-Route::get('getintouch',[GetInTouchController::class, 'getintouch'])->name('getintouch');
-
-Route::get('/', [DashboardController::class, 'index'])->name('home');
-Route::get('/admin/contohtabel', [AdminController::class, 'tabel']);
-Route::resource('pegawai',PegawaiController::class);
-Route::resource('kategoriproduk',KategoriProdukController::class);
-Route::resource('member',MemberController::class);
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
+// Route::get('/login', [LoginController::class, 'index']);
+// Route::post('/login', [LoginController::class, 'authenticate']);
+
 Auth::routes();
-// Route::get('/admin/dashboard', [App\Http\Controllers\AdminController::class, 'dashboard'])->name('home');
-// Auth::logout();
-// Route::get('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('home');
+    Route::resource('user', UserController::class);
+    Route::resource('pegawai', PegawaiController::class);
+    Route::resource('kategoriproduk', KategoriProdukController::class);
+    Route::get('getintouch', [GetInTouchController::class, 'getintouch'])->name('getintouch');
+    Route::resource('member', MemberController::class);
+});
+
+Route::get('/klien-beranda', [KlienController::class, 'home']);
+Route::get('/klien-produk', [KlienController::class, 'produk']);
+Route::get('/klien-galery', [KlienController::class, 'galery']);
+Route::get('/klien-about', [KlienController::class, 'about']);
+Route::resource('contact', GetInTouchController::class);
