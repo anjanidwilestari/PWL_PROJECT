95c5a8fc619eb653de33da8ba8ed90449b0969dc
bellasoniadwi <bellasoniadwi@gmail.com>
2022-06-07 12:14:29 +0700

    update footer tampilan member

--- resources/views/klienLayouts/footer.blade.php
+++ resources/views/klienLayouts/footer.blade.php
@@ -24,9 +24,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="/klien-contact">Hubungi</a>
                         </li>
-                        <li class="nav-item">
+                        {{-- <li class="nav-item">
                             <a class="nav-link" href="#">Login</a>
-                        </li>
+                        </li> --}}
                     </ul>
                 </div>
             </div>
