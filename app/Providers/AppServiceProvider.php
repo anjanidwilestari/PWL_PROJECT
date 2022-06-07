b6a43e66465c04b785763431f80ef2ceb833b886
bellasoniadwi <bellasoniadwi@gmail.com>
2022-06-07 17:24:42 +0700

    add gate untuk otorisasi admin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +27,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+
+        Gate::define('admin', function(User $user){
+            return $user->jabatan === 'Admin';
+        });
     }
 }
