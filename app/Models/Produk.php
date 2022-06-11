128bc08c62f22d711ce93feb6ce243e130cc8602
Ayi <ayianjani774@gmail.com>
2022-06-11 14:13:03 +0700

    update model

--- app/Models/Produk.php
+++ app/Models/Produk.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\KategoriProduk;
 
 class Produk extends Model
 {
     use HasFactory;
+    protected $table ='produks';
+    protected $primaryKey = 'id';
+
+    protected $fillable =[
+        'kategori_id',
+        'nama_produk',
+        'kode_produk',
+        'gambar',
+        'biaya_per_hari',
+    ];
+
+    public function kategoriproduk(){
+        return $this->belongsTo(KategoriProduk::class);
+    }
 }
