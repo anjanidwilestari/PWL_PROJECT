20ef37d6983c30b0b9351901eae91f0f9e44d79f
Ayi <ayianjani774@gmail.com>
2022-06-11 14:12:52 +0700

    update relasi

--- app/Models/KategoriProduk.php
+++ app/Models/KategoriProduk.php
@@ -4,14 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Produk;
 
 class KategoriProduk extends Model
 {
     use HasFactory;
     protected $table = 'kategori_produks';
+    protected $primaryKey = 'id';
 
     protected $fillable = [
         'nama_kategori',
         'deskripsi',
     ];
+
+    public function produk(){
+        return $this->hasMany(Produk::class);
+    }
 }
