7f50c333a87ed33b60e01a0d619793a800805c2f
bellasoniadwi <bellasoniadwi@gmail.com>
2022-06-12 13:26:48 +0700

    create migration,model peminjaman

---
+++ app/Models/Peminjaman.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Member;
+use App\Models\Produk;
+
+class Peminjaman extends Model
+{
+    use HasFactory;
+
+    protected $table = 'peminjamans';
+    
+    protected $fillable = [
+        'member_id',
+        'produk_id',
+        'nama_petugas',
+        'jumlah_pinjam',
+        'total_harga',
+        'tgl_pinjam',
+        'lama_pinjam',
+        'status'
+    ];
+
+    public function member()
+    {
+        return $this->belongsTo(Member::class);
+    }
+
+    public function produk()
+    {
+        return $this->belongsTo(Produk::class);
+    }
+
+}
