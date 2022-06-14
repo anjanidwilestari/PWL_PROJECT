c5b6279b0208985f4dd82de9dee06de3f44de3e7
bellasoniadwi <bellasoniadwi@gmail.com>
2022-06-14 15:56:10 +0700

    modify alert

--- resources/views/peminjaman/peminjamanindex.blade.php
+++ resources/views/peminjaman/peminjamanindex.blade.php
@@ -143,7 +143,7 @@
 @endsection
 @section('js')
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js"></script>
+{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js"></script> --}}
 <script type="text/javascript">
  
      $('.show_confirm').click(function(event) {
@@ -160,6 +160,8 @@
           .then((willDelete) => {
             if (willDelete) {
               form.submit();
+            } else {
+                swal("Data Anda Aman!");
             }
           });
       });
