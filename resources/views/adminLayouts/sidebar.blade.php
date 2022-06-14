791a37dbed087d044a5f504e1d14a198d6eaab8d
bellasoniadwi <bellasoniadwi@gmail.com>
2022-06-14 16:02:05 +0700

    modify dikit

--- resources/views/adminLayouts/sidebar.blade.php
+++ resources/views/adminLayouts/sidebar.blade.php
@@ -105,15 +105,6 @@
               }
             }
             
-            function closeFullscreen() {
-              if (document.exitFullscreen) {
-                document.exitFullscreen();
-              } else if (document.webkitExitFullscreen) { /* Safari */
-                document.webkitExitFullscreen();
-              } else if (document.msExitFullscreen) { /* IE11 */
-                document.msExitFullscreen();
-              }
-            }
             </script>
     </div>
     <!-- /menu footer buttons -->
